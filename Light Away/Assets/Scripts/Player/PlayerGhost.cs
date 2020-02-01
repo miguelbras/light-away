18a2c80bfe24600bc1d5a1b301cb5ebc6b8025ba
Tibério <tiberio.baptista@ua.pt>
2020-02-01 17:35:57 +0000

    It's good now

--- Light Away/Assets/Scripts/Player/PlayerGhost.cs
+++ Light Away/Assets/Scripts/Player/PlayerGhost.cs
@@ -76,9 +76,60 @@ public class PlayerGhost : Player
         isGhost = !isGhost;
     }
 
+    private void turnIntoGhost()
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        isGhost = true;
+    }
+
+    private void turnIntoHuman()
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        isGhost = false;
+    }
+
     private void fireAction()
     {
         Debug.Log("Ghost pressing things");
     }
+
+    private void checkIfIsInLight()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Light")
+        {
+            if(coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+
+            if (Physics2D.OverlapCircle(transform.position, .1f, ground) == null)
+            {
+                Debug.Log("exited light222");
+                coroutine = StartCoroutine(LeaveGhost());
+                turnIntoHuman();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("exited light");
+        //if (collider.gameObject.tag == "Light")
+        //    StartCoroutine(coroutine);
+        //Invoke("turnIntoGhost", 5);
+    }
+
+    private IEnumerator LeaveGhost()
+    {
+        yield return new WaitForSeconds(4);
+        turnIntoGhost();
+    }
+
+
 
 }
---
+++ Light Away/Assets/Scripts/World/WorldObjects/Lever.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField]
+    PICK_UP[] required;
+
+    bool isActivated = false;
+
+    [SerializeField]
+    GameObject worldManager;
+
+    [SerializeField]
+    GameObject requiredBubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void interact()
+    {
+
+    }
+
+}
